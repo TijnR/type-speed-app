3455c1b5c5825fcf1cbe3f195f808436d2dfac0e
Tijn Roozen <tijnroozen@gmail.com>
2021-03-08 10:41:59 +0100

    Build setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './speed-type.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    bruh
+    </div>
+  );
+}
+
+export default App;
